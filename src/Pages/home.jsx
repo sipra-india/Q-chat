072afbc32793e56c8476451b9d47e1126c72bc4f
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-08-20 19:30:21 +0530

    home page started

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from './components/sidebar';
 
 export default function Home() {
   return (
     <div>
         <div className='container'>
+          <Sidebar/>
         </div>
     </div>
   )
