d45376cc5f19008edf732e30866f3772acc4fd3b
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-08-23 19:59:52 +0530

    user started

--- src/Pages/components/user.jsx
+++ src/Pages/components/user.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import pfp from '../../images/pfp1.jpg';
+import './user.css';
 
 export default function User() {
   return (
-    <div>User</div>
+    <div className='chat'>
+        <img id='img' src={pfp} alt=''/>
+        <div className='info'>
+            <span>name</span>
+            <span>last message</span>
+        </div>
+    </div>
   )
 }
