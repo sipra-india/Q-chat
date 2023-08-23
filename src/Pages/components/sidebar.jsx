63c2f689cdabbcfd80b2fdb530846a92fada31a4
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-08-23 19:42:13 +0530

    zooming problem solved

--- src/Pages/components/sidebar.jsx
+++ src/Pages/components/sidebar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './sidebar.css';
 import UserInfo from './UserInfo';
 import Search from './search';
+import User from './user';
 
 export default function Sidebar() {
   return (
     <div className='main'>
       <UserInfo/>
       <Search/>
+      <User/>
     </div>
   )
 }
