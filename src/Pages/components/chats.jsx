e97a2e067b2010acac1e2253eb1a84982ebc7ef8
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-08-24 19:15:15 +0530

    trying to color the options

--- src/Pages/components/chats.jsx
+++ src/Pages/components/chats.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './chats.css';
-import pfp from '../../images/pfp1.jpg';
-import more from '../../images/more.png';
-import cam from '../../images/camera.png';
-import add from '../../images/add.png';
+import more from '../../images/more.svg';
+import cam from '../../images/cam.svg';
+import add from '../../images/add.svg';
 
 export default function Chat_Area() {
   return (
     <div className='chats'>
       <div className="chat-info">
-        <img src={pfp} alt=''/>
-        <span>Name</span>
-        <img src={cam}/>
-        <img src={add} />
-        <img src={more} />
+        <div id="username">
+        <span id='name'>Name</span>
+        </div>
+        <div id="options">
+        <img className='img' src={cam}/>
+        <img className='img' src={add} />
+        <img className='img' src={more} />
+        </div>
       </div>
     </div>
   )
