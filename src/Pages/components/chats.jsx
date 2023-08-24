1f28bd5611b8acaadaaf706c0a2c0ec58c82a758
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-08-24 18:28:39 +0530

    startes the chat area

--- src/Pages/components/chats.jsx
+++ src/Pages/components/chats.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import './chats.css'
+import './chats.css';
+import pfp from '../../images/pfp1.jpg';
+import more from '../../images/more.png';
+import cam from '../../images/camera.png';
+import add from '../../images/add.png';
 
 export default function Chat_Area() {
   return (
-    <div className='chats'>Chat_Area</div>
+    <div className='chats'>
+      <div className="chat-info">
+        <img src={pfp} alt=''/>
+        <span>Name</span>
+        <img src={cam}/>
+        <img src={add} />
+        <img src={more} />
+      </div>
+    </div>
   )
 }
